154a4d016d5590125304eb58e05f46a5a6860a37
Jeeglobs <dp.hudgins@hotmail.com>
2023-03-18 14:37:43 -0400

    created Record Model and RecordForm

--- core/models.py
+++ core/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from datetime import date
 
 
 class User(AbstractUser):
     pass
+
+
+class Habit(models.Model):
+    name = models.CharField(max_length=100)
+    target = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Record(models.Model):
+    date = models.DateField(default=date.today)
+    number_completed = models.PositiveIntegerField()
